1d013b7baa09ff099c64fd1525eaf4091b550a7a
Varun Chandan <52221719+varunchandan2@users.noreply.github.com>
2023-07-07 20:28:28 -0700

    organize

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,14 @@ func main() {
 
 }
 
+//Extra Notes
+
+/*
+	// Type conversion for any data
+	greeting := "Hello World!"
+	fmt.Println([]byte(greeting))
+*/
+
 /*
 Slice arrays all should have the same data type and can grow and shrink
 Arrays defined has defined length, and you have to declare it everytime you use
